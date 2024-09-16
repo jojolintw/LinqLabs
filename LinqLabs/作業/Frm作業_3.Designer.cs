83237efaeb9795ddd5e23e8938248f092123442b
jojo <User@DESKTOP-RAE7E6C>
2024-09-16 10:50:47 +0800

    3-1

--- LinqLabs/作業/Frm作業_3.Designer.cs
+++ LinqLabs/作業/Frm作業_3.Designer.cs
@@ -35,6 +35,14 @@
             this.button38 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.button33 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button36
@@ -75,7 +83,7 @@
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button6.ForeColor = System.Drawing.Color.Black;
-            this.button6.Location = new System.Drawing.Point(534, 106);
+            this.button6.Location = new System.Drawing.Point(534, 109);
             this.button6.Margin = new System.Windows.Forms.Padding(5);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(416, 42);
@@ -87,7 +95,7 @@
             // 
             this.button38.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button38.ForeColor = System.Drawing.Color.Black;
-            this.button38.Location = new System.Drawing.Point(539, 54);
+            this.button38.Location = new System.Drawing.Point(534, 54);
             this.button38.Margin = new System.Windows.Forms.Padding(5);
             this.button38.Name = "button38";
             this.button38.Size = new System.Drawing.Size(416, 42);
@@ -116,11 +124,114 @@
             this.button33.UseVisualStyleBackColor = false;
             this.button33.Click += new System.EventHandler(this.button33_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(529, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 18);
+            this.label3.TabIndex = 158;
+            this.label3.Text = "LINQ to Northwind Entity";
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button10.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(532, 331);
+            this.button10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(418, 35);
+            this.button10.TabIndex = 157;
+            this.button10.Text = " Orders -  Group by 年 / 月";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button9.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(534, 464);
+            this.button9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(418, 35);
+            this.button9.TabIndex = 149;
+            this.button9.Text = "     NW 產品最高單價前 5 筆 (包括類別名稱)";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(534, 391);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(418, 37);
+            this.button2.TabIndex = 155;
+            this.button2.Text = "總銷售金額";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button7.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(534, 500);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(418, 35);
+            this.button7.TabIndex = 150;
+            this.button7.Text = "     NW 產品有任何一筆單價大於300 ?";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button8.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.Black;
+            this.button8.Location = new System.Drawing.Point(534, 245);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(418, 35);
+            this.button8.TabIndex = 151;
+            this.button8.Text = "NW Products 低中高 價產品 ";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(534, 428);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(418, 35);
+            this.button1.TabIndex = 153;
+            this.button1.Text = "銷售最好的top 5業務員";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button15.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.Location = new System.Drawing.Point(534, 290);
+            this.button15.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(418, 35);
+            this.button15.TabIndex = 152;
+            this.button15.Text = " Orders -  Group by 年";
+            this.button15.UseVisualStyleBackColor = false;
+            // 
             // Frm作業_3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(995, 506);
+            this.ClientSize = new System.Drawing.Size(1111, 583);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button15);
             this.Controls.Add(this.button33);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -144,5 +255,13 @@
         private System.Windows.Forms.Button button38;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button15;
     }
 }
--- LinqLabs/作業/Frm作業_3.cs
+++ LinqLabs/作業/Frm作業_3.cs
@@ -12,42 +12,111 @@ namespace LinqLabs.作業
 {
     public partial class Frm作業_3 : Form
     {
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Class { get; set; }
+            public int Chi { get; set; } 
+            public int Eng { get; set; } 
+            public int Math { get; set; }
+            public string Gender { get; set; }
+        }
+        private List<Student> _stuScores;
         public Frm作業_3()
         {
             InitializeComponent();
-
-            //hint
-            //students_scores = new List<Student>()
-            //                             {
-            //                                new Student{ Name = "aaa", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Male" },
-            //                                new Student{ Name = "bbb", Class = "CS_102", Chi = 80, Eng = 80, Math = 100, Gender = "Male" },
-            //                                new Student{ Name = "ccc", Class = "CS_101", Chi = 60, Eng = 50, Math = 75, Gender = "Female" },
-            //                                new Student{ Name = "ddd", Class = "CS_102", Chi = 80, Eng = 70, Math = 85, Gender = "Female" },
-            //                                new Student{ Name = "eee", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Female" },
-            //                                new Student{ Name = "fff", Class = "CS_102", Chi = 80, Eng = 80, Math = 80, Gender = "Female" },
-
-            //                              };
+            _stuScores = new List<Student>()
+                    {
+                    new Student{ Name = "aaa", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Male" },
+                    new Student{ Name = "bbb", Class = "CS_102", Chi = 80, Eng = 80, Math = 100, Gender = "Male" },
+                    new Student{ Name = "ccc", Class = "CS_101", Chi = 60, Eng = 50, Math = 75, Gender = "Female" },
+                    new Student{ Name = "ddd", Class = "CS_102", Chi = 80, Eng = 70, Math = 85, Gender = "Female" },
+                    new Student{ Name = "eee", Class = "CS_101", Chi = 80, Eng = 80, Math = 50, Gender = "Female" },
+                    new Student{ Name = "fff", Class = "CS_102", Chi = 80, Eng = 80, Math = 80, Gender = "Female" },
+                    };
         }
-    
+
         private void button36_Click(object sender, EventArgs e)
         {
             #region 搜尋 班級學生成績
+            var sb = new StringBuilder();
 
-            // 
-            // 共幾個 學員成績 ?						
+
+            // 共幾個 學員成績 ?		
+            var q = _stuScores.Count();
+            sb.AppendLine($"Total students: {q}");
 
             // 找出 前面三個 的學員所有科目成績					
-            // 找出 後面兩個 的學員所有科目成績					
+            var firstThreeStudents = _stuScores.Take(3);
+            sb.AppendLine("前面三個學員所有科目成績:");
+            foreach (var student in firstThreeStudents)
+            {
+                sb.AppendLine($"Name: {student.Name}, Chi: {student.Chi}, Eng: {student.Eng}, Math: {student.Math}");
+            }
 
-            // 找出 Name 'aaa','bbb','ccc' 的學員國文英文科目成績						
 
-            // 找出學員 'bbb' 的成績	                          
+            // 找出 後面兩個 的學員所有科目成績			
+            var lastTwo = _stuScores.Skip(_stuScores.Count() - 2);
+            foreach (var student in lastTwo)
+            {
+                sb.AppendLine($"{student.Name} - Chi: {student.Chi}, Eng: {student.Eng}, Math: {student.Math}");
+            }
+
+            // 找出 Name 'aaa','bbb','ccc' 的學員國文英文科目成績	
+            var selectedStudents1 = _stuScores.Where(s => s.Name == "aaa" || s.Name == "bbb" || s.Name == "ccc");
+            foreach (var student in selectedStudents1)
+            {
+                sb.AppendLine($"{student.Name} - Chi: {student.Chi}, Eng: {student.Eng}");
+            }
+
+            // 找出學員 'bbb' 的成績
+            var bbbScore = _stuScores.FirstOrDefault(s => s.Name == "bbb");
+            if (bbbScore != null)
+            {
+                sb.AppendLine($"bbb's Scores - Chi: {bbbScore.Chi}, Eng: {bbbScore.Eng}, Math: {bbbScore.Math}");
+            }
 
             // 找出除了 'bbb' 學員的學員的所有成績 ('bbb' 退學)	
+            var otherStudents = _stuScores.Where(s => s.Name != "bbb");
+            sb.AppendLine("除了 'bbb' 學員的所有成績:");
+            foreach (var student in otherStudents)
+            {
+                sb.AppendLine($"Name: {student.Name}, Chi: {student.Chi}, Eng: {student.Eng}, Math: {student.Math}");
+            }
 
-            // 找出 'aaa', 'bbb' 'ccc' 學員 國文數學兩科 科目成績  |				
+            // 找出 'aaa', 'bbb' 'ccc' 學員 國文數學兩科 科目成績  |	
+            var specificStudentsScores = _stuScores
+                                        .Where(s => s.Name == "aaa" || s.Name == "bbb" || s.Name == "ccc")
+                                        .Select(s => new
+                                        {
+                                        s.Name,
+                                        s.Chi,
+                                        s.Math
+                                        });
+
+            sb.AppendLine("Name 為 'aaa', 'bbb', 'ccc' 的學員國文和數學科目成績:");
+            foreach (var student in specificStudentsScores)
+            {
+                sb.AppendLine($"Name: {student.Name}, 國文: {student.Chi}, 數學: {student.Math}");
+            }
             // 數學不及格 ... 是誰 
+            var studentsFailingMath = _stuScores
+                                        .Where(s => s.Math < 60)
+                                        .Select(s => new
+                                        {
+                                            s.Name,
+                                            s.Math
+                                        });
+
+            sb.AppendLine("數學不及格的學員:");
+            foreach (var student in studentsFailingMath)
+            {
+                sb.AppendLine($"Name: {student.Name}, 數學成績: {student.Math}");
+            }
             #endregion
+
+            MessageBox.Show(sb.ToString(), "學員成績結果");
+
         }
 
         private void button37_Click(object sender, EventArgs e)
