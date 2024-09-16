b3042d4ca41f33135169114a5335d1a70ab62ab7
jojo <User@DESKTOP-RAE7E6C>
2024-09-16 13:33:00 +0800

    1330

--- LinqLabs/作業/Frm作業_3.Designer.cs
+++ LinqLabs/作業/Frm作業_3.Designer.cs
@@ -43,15 +43,17 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button36
             // 
             this.button36.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button36.Location = new System.Drawing.Point(81, 54);
-            this.button36.Margin = new System.Windows.Forms.Padding(4);
+            this.button36.Location = new System.Drawing.Point(61, 43);
             this.button36.Name = "button36";
-            this.button36.Size = new System.Drawing.Size(332, 39);
+            this.button36.Size = new System.Drawing.Size(249, 31);
             this.button36.TabIndex = 143;
             this.button36.Text = "搜尋 班級學生成績";
             this.button36.UseVisualStyleBackColor = false;
@@ -60,10 +62,9 @@
             // button37
             // 
             this.button37.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button37.Location = new System.Drawing.Point(81, 109);
-            this.button37.Margin = new System.Windows.Forms.Padding(4);
+            this.button37.Location = new System.Drawing.Point(61, 87);
             this.button37.Name = "button37";
-            this.button37.Size = new System.Drawing.Size(332, 39);
+            this.button37.Size = new System.Drawing.Size(249, 31);
             this.button37.TabIndex = 142;
             this.button37.Text = "每個學生個人成績";
             this.button37.UseVisualStyleBackColor = false;
@@ -72,10 +73,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(551, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(413, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 15);
+            this.label1.Size = new System.Drawing.Size(92, 12);
             this.label1.TabIndex = 146;
             this.label1.Text = "LINQ to FileInfo[]";
             // 
@@ -83,42 +83,42 @@
             // 
             this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button6.ForeColor = System.Drawing.Color.Black;
-            this.button6.Location = new System.Drawing.Point(534, 109);
-            this.button6.Margin = new System.Windows.Forms.Padding(5);
+            this.button6.Location = new System.Drawing.Point(400, 87);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(416, 42);
+            this.button6.Size = new System.Drawing.Size(312, 34);
             this.button6.TabIndex = 144;
             this.button6.Text = "  依 年 分組檔案 (大=>小)";
             this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button38
             // 
             this.button38.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.button38.ForeColor = System.Drawing.Color.Black;
-            this.button38.Location = new System.Drawing.Point(534, 54);
-            this.button38.Margin = new System.Windows.Forms.Padding(5);
+            this.button38.Location = new System.Drawing.Point(400, 43);
+            this.button38.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button38.Name = "button38";
-            this.button38.Size = new System.Drawing.Size(416, 42);
+            this.button38.Size = new System.Drawing.Size(312, 34);
             this.button38.TabIndex = 145;
             this.button38.Text = "依 檔案大小 分組檔案 (大=>小)";
             this.button38.UseVisualStyleBackColor = false;
+            this.button38.Click += new System.EventHandler(this.button38_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(88, 21);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(66, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 15);
+            this.label2.Size = new System.Drawing.Size(111, 12);
             this.label2.TabIndex = 147;
             this.label2.Text = "LINQ to List<Student>";
             // 
             // button33
             // 
-            this.button33.Location = new System.Drawing.Point(81, 174);
-            this.button33.Margin = new System.Windows.Forms.Padding(4);
+            this.button33.Location = new System.Drawing.Point(61, 139);
             this.button33.Name = "button33";
-            this.button33.Size = new System.Drawing.Size(332, 37);
+            this.button33.Size = new System.Drawing.Size(249, 30);
             this.button33.TabIndex = 148;
             this.button33.Text = "隨機 統計 100 個學生 分數";
             this.button33.UseVisualStyleBackColor = false;
@@ -128,9 +128,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(529, 211);
+            this.label3.Location = new System.Drawing.Point(397, 169);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(200, 18);
+            this.label3.Size = new System.Drawing.Size(175, 14);
             this.label3.TabIndex = 158;
             this.label3.Text = "LINQ to Northwind Entity";
             // 
@@ -138,22 +139,23 @@
             // 
             this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button10.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(532, 331);
-            this.button10.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button10.Location = new System.Drawing.Point(399, 265);
+            this.button10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(418, 35);
+            this.button10.Size = new System.Drawing.Size(314, 28);
             this.button10.TabIndex = 157;
             this.button10.Text = " Orders -  Group by 年 / 月";
             this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button9
             // 
             this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button9.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.Location = new System.Drawing.Point(534, 464);
-            this.button9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button9.Location = new System.Drawing.Point(400, 371);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(418, 35);
+            this.button9.Size = new System.Drawing.Size(314, 28);
             this.button9.TabIndex = 149;
             this.button9.Text = "     NW 產品最高單價前 5 筆 (包括類別名稱)";
             this.button9.UseVisualStyleBackColor = false;
@@ -162,22 +164,23 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(534, 391);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(400, 313);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(418, 37);
+            this.button2.Size = new System.Drawing.Size(314, 30);
             this.button2.TabIndex = 155;
             this.button2.Text = "總銷售金額";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button7
             // 
             this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button7.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(534, 500);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button7.Location = new System.Drawing.Point(400, 400);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(418, 35);
+            this.button7.Size = new System.Drawing.Size(314, 28);
             this.button7.TabIndex = 150;
             this.button7.Text = "     NW 產品有任何一筆單價大於300 ?";
             this.button7.UseVisualStyleBackColor = false;
@@ -187,22 +190,23 @@
             this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button8.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.ForeColor = System.Drawing.Color.Black;
-            this.button8.Location = new System.Drawing.Point(534, 245);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button8.Location = new System.Drawing.Point(400, 196);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(418, 35);
+            this.button8.Size = new System.Drawing.Size(314, 28);
             this.button8.TabIndex = 151;
             this.button8.Text = "NW Products 低中高 價產品 ";
             this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(534, 428);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(400, 342);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(418, 35);
+            this.button1.Size = new System.Drawing.Size(314, 28);
             this.button1.TabIndex = 153;
             this.button1.Text = "銷售最好的top 5業務員";
             this.button1.UseVisualStyleBackColor = false;
@@ -211,19 +215,38 @@
             // 
             this.button15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.button15.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button15.Location = new System.Drawing.Point(534, 290);
-            this.button15.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button15.Location = new System.Drawing.Point(400, 232);
+            this.button15.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button15.Name = "button15";
-            this.button15.Size = new System.Drawing.Size(418, 35);
+            this.button15.Size = new System.Drawing.Size(314, 28);
             this.button15.TabIndex = 152;
             this.button15.Text = " Orders -  Group by 年";
             this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 184);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(381, 270);
+            this.treeView1.TabIndex = 159;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(746, 14);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 440);
+            this.dataGridView1.TabIndex = 160;
             // 
             // Frm作業_3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1111, 583);
+            this.ClientSize = new System.Drawing.Size(1370, 478);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
@@ -239,8 +262,10 @@
             this.Controls.Add(this.button38);
             this.Controls.Add(this.button36);
             this.Controls.Add(this.button37);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Frm作業_3";
             this.Text = "Frm作業_3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,5 +288,7 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- LinqLabs/作業/Frm作業_3.cs
+++ LinqLabs/作業/Frm作業_3.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -123,7 +124,24 @@ namespace LinqLabs.作業
         {
             //個人 sum, min, max, avg
             // 統計 每個學生個人成績 並排序
-           
+            var studentAnalysis = _stuScores.Select(s => new
+            {
+                s.Name,
+                Sum = s.Chi + s.Eng + s.Math,
+                Avg = Math.Round((decimal)((s.Chi + s.Eng + s.Math) / 3), 1),
+                Max = new[] { s.Chi, s.Eng, s.Math }.Max(),
+                Min = new[] { s.Chi, s.Eng, s.Math }.Min(),
+            }).OrderByDescending(s => s.Avg);
+
+            var sb = new StringBuilder();
+            sb.AppendLine("學生成績統計:");
+            foreach (var student in studentAnalysis)
+            {
+                sb.AppendLine($"Name: {student.Name}, 總分: {student.Sum}, 平均: {student.Avg}, 最高分: {student.Max}, 最低分: {student.Min}");
+            }
+
+            // 顯示結果
+            MessageBox.Show(sb.ToString(), "學生成績分析結果");
 
         }
 
@@ -131,6 +149,235 @@ namespace LinqLabs.作業
         {
             // split=> 分成 三群 '待加強'(60~69) '佳'(70~89) '優良'(90~100) 
             // print 每一群是哪幾個 ? (每一群 sort by 分數 descending)
+            var groups = _stuScores.Select(s => new
+            {
+                s.Name,
+                Avg = (s.Chi + s.Eng + s.Math) / 3.0
+            }).GroupBy(s =>
+            {
+                if (s.Avg >= 90) return "優良";
+                if (s.Avg >= 70) return "佳";
+                return "待加強";
+            }).ToList();
+
+            // 排序並顯示在 treeView1
+            treeView1.Nodes.Clear();
+
+            foreach (var group in groups.OrderBy(g => g.Key))
+            {
+                var groupNode = new TreeNode(group.Key);
+
+                foreach (var student in group.OrderByDescending(s => s.Avg))
+                {
+                    var studentNode = new TreeNode($"{student.Name}: {student.Avg:F1}");
+                    groupNode.Nodes.Add(studentNode);
+                }
+
+                treeView1.Nodes.Add(groupNode);
+            }
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            // 取得指定目錄下的檔案
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            // 分組檔案，根據檔案大小分類
+            var groupedFiles = from f in files
+                               group f by f.Length > 10000 ? "大檔案" : "小檔案" into g
+                               select new
+                               {
+                                   FileType = g.Key,
+                                   Files = g.OrderByDescending(file => file.Length).ToList() 
+                               };
+
+            // 顯示在 DataGridView
+            this.dataGridView1.DataSource = groupedFiles.ToList();
+
+            // 清除 TreeView 中現有的節點
+            this.treeView1.Nodes.Clear();
+
+            // 將分組結果顯示在 TreeView
+            foreach (var group in groupedFiles)
+            {
+                // 為每個檔案類別建立一個節點
+                TreeNode parentNode = new TreeNode(group.FileType);
+
+                // 將檔案列表添加為子節點
+                foreach (var file in group.Files)
+                {
+                    parentNode.Nodes.Add(new TreeNode(file.Name));
+                }
+
+                // 將每個檔案類別節點添加到 TreeView
+                this.treeView1.Nodes.Add(parentNode);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"c:\windows");
+            System.IO.FileInfo[] files = dir.GetFiles();
+
+            var groupedYears = from f in files
+                               group f by f.CreationTime.Year into g
+                               select new
+                               {
+                                   Year = g.Key,
+                                   FileCount = g.Count(),
+                                   AvgFileSize = g.Average(f => f.Length),
+                                   Files = g.OrderByDescending(f => f.Length).ToList()
+                               };
+            this.dataGridView1.DataSource = groupedYears.ToList();
+            this.treeView1.Nodes.Clear();
+            foreach (var group in groupedYears.OrderByDescending(g => g.Year))
+            {
+                // 為每個年份建立一個節點
+                TreeNode parentNode = new TreeNode($"{group.Year} ({group.FileCount} files)");
+
+                // 將檔案列表添加為子節點
+                foreach (var file in group.Files)
+                {
+                    parentNode.Nodes.Add(new TreeNode($"{file.Name} ({file.Length / 1024} KB)")); // 顯示檔案名稱和大小（以KB為單位）
+                }
+
+                // 將每個年份節點添加到 TreeView
+                this.treeView1.Nodes.Add(parentNode);
+            }
+
+        }
+        NorthwindEntities dbContext = new NorthwindEntities();
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // 先從資料庫中獲取所有產品
+            var products = dbContext.Products.ToList();
+
+            // 在記憶體中進行分組
+            var q = from p in products
+                    group p by price(p.UnitPrice) into g
+                    select new
+                    {
+                        PriceGroup = g.Key,
+                        Products = g.ToList()
+                    };
+
+            // 設置 DataGridView 的資料來源
+            this.dataGridView1.DataSource = q.ToList();
+
+            // 清空 TreeView 的節點
+            this.treeView1.Nodes.Clear();
+
+            // 將分組結果顯示在 TreeView 中
+            foreach (var group in q)
+            {
+                TreeNode parentNode = new TreeNode(group.PriceGroup);
+                foreach (var product in group.Products)
+                {
+                    parentNode.Nodes.Add($"{product.ProductName} ({product.UnitPrice:C})");
+                }
+                this.treeView1.Nodes.Add(parentNode);
+            }
+        }
+
+        private string price(decimal? unitPrice)
+        {
+            // 檢查 unitPrice 是否為 null，並根據值返回對應的價格區間
+            if (unitPrice == null)
+                return "未知";
+
+            if (unitPrice < 20)
+                return "低價";
+            else if (unitPrice >= 20 && unitPrice < 40)
+                return "中價";
+            else
+                return "高價";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            var order = dbContext.Orders.ToList();
+            var q = from p in order
+                    group p by p.OrderDate.Value.Year into g
+                    select new
+                    {
+                        Year = g.Key,
+                        Orders = g.ToList()
+                    };
+            this.dataGridView1.DataSource = q.ToList();
+            this.treeView1.Nodes.Clear();
+            foreach (var group in q)
+            {
+                // 創建年份節點
+                TreeNode yearNode = new TreeNode(group.Year.ToString());
+
+                foreach (var orders in group.Orders)
+                {
+                    // 將訂單信息添加到年份節點下
+                    yearNode.Nodes.Add($"Order ID: {orders.OrderID}, Order Date: {orders.OrderDate}");
+                }
+
+                // 將年份節點添加到 TreeView
+                this.treeView1.Nodes.Add(yearNode);
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            var orders = dbContext.Orders.ToList();
+
+            // 根據年份和月份分組
+            var groupedOrders = from o in orders
+                                group o by new { Year = o.OrderDate.Value.Year, Month = o.OrderDate.Value.Month } into g
+                                orderby g.Key.Year, g.Key.Month // 按年份和月份排序
+                                select new
+                                {
+                                    Year = g.Key.Year,
+                                    Month = g.Key.Month,
+                                    Orders = g.ToList()
+                                };
+
+            // 將結果綁定到 DataGridView
+            this.dataGridView1.DataSource = groupedOrders.ToList();
+
+            // 清空 TreeView 的節點
+            this.treeView1.Nodes.Clear();
+
+            // 先按年份分組
+            var years = groupedOrders.GroupBy(g => g.Year).OrderBy(g => g.Key);
+
+            foreach (var yearGroup in years)
+            {
+                // 創建年份節點
+                TreeNode yearNode = new TreeNode($"{yearGroup.Key}年");
+
+                // 在年份節點下按月份分組
+                var months = yearGroup.GroupBy(g => g.Month).OrderBy(g => g.Key);
+
+                foreach (var monthGroup in months)
+                {
+                    // 創建月份節點
+                    TreeNode monthNode = new TreeNode($"{monthGroup.Key}月");
+
+                    foreach (var order in monthGroup.SelectMany(g => g.Orders))
+                    {
+                        // 將訂單信息添加到月份節點下
+                        monthNode.Nodes.Add($"Order ID: {order.OrderID}, Order Date: {order.OrderDate}");
+                    }
+
+                    // 將月份節點添加到年份節點下
+                    yearNode.Nodes.Add(monthNode);
+                }
+
+                // 將年份節點添加到 TreeView
+                this.treeView1.Nodes.Add(yearNode);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
